cd021c0fddd8f7e8e510d08ddfa67a64f2ad5c50
“suyuwen” <2982780802@qq.com>
2025-02-12 00:48:34 +0800

    框架搭建

---
+++ course-service/src/main/java/sspu/test/CourseApplication.java
@@ -0,0 +1,11 @@
+package sspu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApplication.class, args);
+    }
+}
---
+++ notice-service/src/main/java/sspu/test/NoticeApplication.java
@@ -0,0 +1,11 @@
+package sspu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeApplication.class, args);
+    }
+}
